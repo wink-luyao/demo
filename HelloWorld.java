cd83d5157bed342c5334be648b9bbedae555c753
wink-luyao <=1551921085@qq.com>
2020-06-16 21:01:42 +0800

    hello commit

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+public static main(String[] args){
+System.out.println("Hellow");
+System.out.println("Hellow");System.out.println("Hellow");System.out.println("Hellow");System.out.println("Hellow");System.out.println("Hellow");System.out.println("Hellow");
+}
+}
